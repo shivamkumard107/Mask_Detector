ad4d06a3ab62e8d5877c271a026e62ba587427f4
Shivam K <shivamk@buyhatke.com>
2020-06-21 02:43:10 +0530

    added articles and paytm

--- app/src/main/java/com/example/mask_detector/fragments/HomeFragment.java
+++ app/src/main/java/com/example/mask_detector/fragments/HomeFragment.java
@@ -1,18 +1,21 @@
 package com.example.mask_detector.fragments;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
-
-import androidx.cardview.widget.CardView;
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import androidx.cardview.widget.CardView;
+import androidx.fragment.app.Fragment;
 
 import com.example.mask_detector.R;
 
 public class HomeFragment extends Fragment {
     private CardView cv1, cv2, cv3;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -22,9 +25,42 @@ public class HomeFragment extends Fragment {
         cv2 = v.findViewById(R.id.cardView3);
         cv3 = v.findViewById(R.id.helpCard3);
 
-        cv1.setOnClickListener(appointment ->{
+        cv1.setOnClickListener(appointment -> {
 
         });
+        cv3.setOnClickListener(read -> {
+
+        });
+        v.findViewById(R.id.helpCard4).setOnClickListener(paytm -> {
+//            net.one97.paytm
+            if (appInstalledOrNot("net.one97.paytm")) {
+                //open bitbns
+                Intent launchIntent = getContext().getPackageManager().getLaunchIntentForPackage("net.one97.paytm");
+                if (launchIntent != null) {
+                    getContext().startActivity(launchIntent);//null pointer check in case package name was not found
+                }
+            } else {
+                openWebsiteOnWeb("https://paytm.com/");
+            }
+        });
         return v;
+    }
+
+    //check and redirect to huobi, gateio, binance and kucoin app in playstore to download
+    private boolean appInstalledOrNot(String uri) {
+        PackageManager pm = getContext().getPackageManager();
+        try {
+            pm.getPackageInfo(uri, PackageManager.GET_ACTIVITIES);
+            return true;
+        } catch (PackageManager.NameNotFoundException ignored) {
+        }
+
+        return false;
+    }
+
+    private void openWebsiteOnWeb(String website) {
+        Intent webIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(website));
+        webIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        getContext().startActivity(webIntent);
     }
 }
