f54495883e8cd831d06167d44c8299f2b7ddc300
swarnima14 <swarnima.tiwari14@gmail.com>
2020-08-08 10:11:23 +0530

    mutiple upload done

--- app/src/main/java/com/example/mask_detector/activities/UploadDocActivity.java
+++ app/src/main/java/com/example/mask_detector/activities/UploadDocActivity.java
@@ -20,6 +20,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.mask_detector.R;
 import com.example.mask_detector.model.Upload;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
@@ -31,6 +32,9 @@ import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.StorageTask;
 import com.google.firebase.storage.UploadTask;
 import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class UploadDocActivity extends AppCompatActivity {
     private static final int PICK_IMAGE_REQUEST = 1;
@@ -44,6 +48,8 @@ public class UploadDocActivity extends AppCompatActivity {
     private StorageReference mStorageRef;
     private DatabaseReference mDatabaseRef;
     private StorageTask mUploadTask;
+    private ArrayList<Uri> ImageList = new ArrayList<Uri>();
+    Integer uploads;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -85,6 +91,8 @@ public class UploadDocActivity extends AppCompatActivity {
         Intent intent = new Intent();
         intent.setType("image/*");
         intent.setAction(Intent.ACTION_GET_CONTENT);
+        intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE,true);
+
         startActivityForResult(intent, PICK_IMAGE_REQUEST);
     }
 
@@ -92,9 +100,30 @@ public class UploadDocActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK
-                && data != null && data.getData() != null) {
-            mImageUri = data.getData();
-            Picasso.get().load(mImageUri).into(mImageView);
+               ) {
+            if (data.getClipData() != null){
+                int count = data.getClipData().getItemCount();
+
+            int CurrentImageSelect = 0;
+
+            while (CurrentImageSelect < count) {
+               // Toast.makeText(this, "count" + count, Toast.LENGTH_SHORT).show();
+                Uri imageuri = data.getClipData().getItemAt(CurrentImageSelect).getUri();
+                ImageList.add(imageuri);
+               // Toast.makeText(this, imageuri.toString(), Toast.LENGTH_SHORT).show();
+                CurrentImageSelect = CurrentImageSelect + 1;
+
+
+            }
+            /*mImageUri = data.getData();*/
+            Picasso.get().load(ImageList.get(0)).into(mImageView);
+        }
+            else if(data.getData()!=null) {
+              //  Toast.makeText(this, "single", Toast.LENGTH_SHORT).show();
+                mImageUri=data.getData();
+                ImageList.add(mImageUri);
+                Picasso.get().load(mImageUri).into(mImageView);
+            }
         }
     }
 
@@ -105,58 +134,70 @@ public class UploadDocActivity extends AppCompatActivity {
     }
 
     private void uploadFile() {
-        if (mImageUri != null) {
-            StorageReference fileReference = mStorageRef.child(System.currentTimeMillis()
-                    + "." + getFileExtension(mImageUri));
-            mUploadTask = fileReference.putFile(mImageUri)
-                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                        @Override
-                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                            Handler handler = new Handler();
-                            handler.postDelayed(new Runnable() {
-                                @Override
-                                public void run() {
-                                    mProgressBar.setProgress(0);
-                                }
-                            }, 500);
-                            Toast.makeText(UploadDocActivity.this, "Upload successful", Toast.LENGTH_LONG).show();
+        if (ImageList != null) {
 
-                            if (taskSnapshot.getMetadata() != null) {
-                                if (taskSnapshot.getMetadata().getReference() != null) {
-                                    Task<Uri> result = taskSnapshot.getStorage().getDownloadUrl();
-                                    result.addOnSuccessListener(new OnSuccessListener<Uri>() {
-                                        @Override
-                                        public void onSuccess(Uri uri) {
-                                            Upload upload = new Upload(mEditTextFileName.getText().toString().trim(),
-                                                    uri.toString());
-                                            String uploadId = mDatabaseRef.push().getKey();
-                                            mDatabaseRef.child(uploadId).setValue(upload);
-                                        }
-                                    });
+            for (uploads = 0; uploads < ImageList.size(); uploads++) {
+                Uri Image = ImageList.get(uploads);
+                //Toast.makeText(this, "check", Toast.LENGTH_SHORT).show();
+                StorageReference fileReference = mStorageRef.child(System.currentTimeMillis()
+                        + "." + getFileExtension(Image));
+                mUploadTask = fileReference.putFile(Image)
+                        .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                            @Override
+                            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                                Handler handler = new Handler();
+                                handler.postDelayed(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        mProgressBar.setProgress(0);
+                                    }
+                                }, 500);
+                                Toast.makeText(UploadDocActivity.this, "Upload successful", Toast.LENGTH_LONG).show();
+
+                                if (taskSnapshot.getMetadata() != null) {
+                                    if (taskSnapshot.getMetadata().getReference() != null) {
+                                        Task<Uri> result = taskSnapshot.getStorage().getDownloadUrl();
+                                        result.addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                            @Override
+                                            public void onSuccess(Uri uri) {
+                                                String url = String.valueOf(uri);
+
+                                                Upload upload = new Upload(mEditTextFileName.getText().toString().trim(),
+                                                        uri.toString());
+                                                String uploadId = mDatabaseRef.push().getKey();
+                                                mDatabaseRef.child(uploadId).setValue(upload);
+                                                ImageList.clear();
+                                            }
+                                        });
+                                    }
                                 }
                             }
-                        }
-                    })
-                    .addOnFailureListener(new OnFailureListener() {
-                        @Override
-                        public void onFailure(@NonNull Exception e) {
-                            Toast.makeText(UploadDocActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
-                        }
-                    })
-                    .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
-                        @Override
-                        public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
-                            double progress = (100.0 * taskSnapshot.getBytesTransferred() / taskSnapshot.getTotalByteCount());
-                            mProgressBar.setProgress((int) progress);
-                        }
-                    });
-        } else {
-            Toast.makeText(this, "No file selected", Toast.LENGTH_SHORT).show();
-        }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(UploadDocActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                            @Override
+                            public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                                double progress = (100.0 * taskSnapshot.getBytesTransferred() / taskSnapshot.getTotalByteCount());
+                                mProgressBar.setProgress((int) progress);
+                            }
+                        });
+            }
+            }
+        else{
+                Toast.makeText(this, "No file selected", Toast.LENGTH_SHORT).show();
+            }
+
     }
 
     private void openImagesActivity() {
         Intent intent = new Intent(this, ShowDocActivity.class);
         startActivity(intent);
     }
+
+
 }
--- app/src/main/java/com/example/mask_detector/auth/LoginOption.java
+++ app/src/main/java/com/example/mask_detector/auth/LoginOption.java
@@ -55,6 +55,10 @@ public class LoginOption extends Fragment {
         user.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+               /* SharedPreferences preferences=getActivity().getPreferences(Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor=preferences.edit();
+                editor.putString("user","not admin");
+                editor.commit();*/
                 phoneNumberFragment phoneNumberFragment = new phoneNumberFragment();
                 getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.container, phoneNumberFragment).addToBackStack(null).commit();
             }
